73901024147642d9be8d54ab9fd91ed3556656ce
minhnhat <minhnhat.dev.21@gmail.com>
2023-12-12 09:48:01 +0700

    handle next

--- music.js
+++ music.js
@@ -17,7 +17,14 @@ playlistContainer.onclick = function(e){
 
 // HANDLE PLAYLIST-------------------------------------------------------------------------------------------------------------
 
+const artistAvatar = $('.artist_avatar')
+const artistName = $('.artist_name')
+const musicTitle = $('.music_title')
 
+const timeEndMusic = $('.timeend_music')
+const timeStartMusic = $('.timestart_music')
+
+// biến trong modalPlaylist
 const imgMusic = $('.playlist_playing-img')
 const nameMusic = $('.playlist_playing-nameMusic')
 const nameArtist = $('.playlist_playing-nameArtist')
@@ -28,6 +35,8 @@ const audio = $('#audio')
 const playBtn = $('.playBtn')
 const pauseBtn = $('.pauseBtn')
 const rangeMusic = $('.timemusic')
+const nextBtn = $('.nextBtn')
+const backBtn = $('.backBtn')
 
 const playlistNext = $('.playlist_next-list')
 const app = {
@@ -89,36 +98,7 @@ const app = {
             path: './assets/music/cigar.mp3',
             image: './assets/img/Nhat Minh.jpg'
         },
-        {
-            count: 7,
-            name: 'Vẫn vậy',
-            singer: 'okenoc',
-            time: `3:42`,
-            path: './assets/music/cigar.mp3',
-            image: './assets/img/Nhat Minh.jpg'
-        },
-        {
-            count: 7,
-            name: 'Vẫn vậy',
-            singer: 'okenoc',
-            time: `3:42`,
-            path: './assets/music/cigar.mp3',
-            image: './assets/img/Nhat Minh.jpg'
-        },
-        {
-            count: 7,
-            name: 'Vẫn vậy',
-            singer: 'okenoc',
-            path: './assets/music/cigar.mp3',
-            image: './assets/img/Nhat Minh.jpg'
-        },
-        {
-            count: 7,
-            name: 'Vẫn vậy',
-            singer: 'okenoc',
-            path: './assets/music/cigar.mp3',
-            image: './assets/img/Nhat Minh.jpg'
-        },
+        
     
     ],
     render(){
@@ -152,6 +132,14 @@ const app = {
     },   
     handleEvent() {
 
+        // Xử lý avatar quay 
+        const avatarAnimate = artistAvatar.animate([
+            {transform : 'rotate(360deg)'}
+        ],{
+            duration: 20000,
+            iterations: Infinity
+        })
+        avatarAnimate.pause()
         // Xử lý kéo đến đâu thì phóng to thu nhỏ đến đấy
         document.onscroll = function (){
             const scrollTop = window.scrollY || document.documentElement.scrollTop
@@ -167,6 +155,7 @@ const app = {
             audio.play()
             playBtn.classList.add('dsNone')
             pauseBtn.classList.add('dsOpen')
+            avatarAnimate.play()
         }
 
         // xử lý khi click pause
@@ -174,11 +163,24 @@ const app = {
             audio.pause()
             pauseBtn.classList.remove('dsOpen')
             playBtn.classList.remove('dsNone')
+            avatarAnimate.pause()
+        }
+
+        // xử lý khi click next
+        nextBtn.onclick = function() {
+            app.nextSong()
+            audio.play()            
+        }
+        // Xử lý khi click back
+        backBtn.onclick = function() {
+            app.backSong()
+            audio.play()
         }
         // khi time tiến độ bài hát thay đổi
         audio.ontimeupdate = function() {
-            rangeMusic.value = ((audio.currentTime / audio.duration) * 100)
+            rangeMusic.value = ((audio.currentTime / audio.duration) * 100)            
         }
+        
         // khi rangeMusic thay đổi 
         rangeMusic.onchange = function(e) {
             audio.currentTime = (e.target.value / 100) * audio.duration
@@ -186,17 +188,38 @@ const app = {
     },
 
     loadCurrentSong(){
+        artistAvatar.src = this.currentSong.image
+        artistName.textContent = this.currentSong.singer
+        musicTitle.textContent = this.currentSong.name
+        timeEndMusic.textContent = this.currentSong.time
         
         imgMusic.src = this.currentSong.image
         nameMusic.textContent = this.currentSong.name
         nameArtist.textContent = this.currentSong.singer
         linkMusic.textContent = this.currentSong.name
         timeMusic.textContent = this.currentSong.time
+        
         audio.src = this.currentSong.path
+        
+        
+        
     },
-    // playCurrentSong(){
-
-    // },
+    nextSong(){
+        this.currentIndex++        
+        if(this.currentIndex == this.songs.length){
+            this.currentIndex = 0
+        }
+        
+        this.loadCurrentSong()
+        console.log(audio.src)    
+    },
+    backSong(){
+        this.currentIndex--
+        if(this.currentIndex < 0){
+            this.currentIndex = this.songs.length - 1
+        }
+        this.loadCurrentSong()   
+    },
     start() {
         // Định nghĩa các thuộc tính cho object app
         this.defineProperties()
