e6ca62472f8543f87773b35a0a0f4c470cd8cb9a
minhnhat <minhnhat.dev.21@gmail.com>
2023-12-16 00:46:47 +0700

    searchInput , volume

--- music.js
+++ music.js
@@ -15,12 +15,24 @@ playlistContainer.onclick = function(e){
     e.stopPropagation()
 }
 
+
+// Xử lý khi click vào input 
+const searchInput = $('.search_input')
+const clearInput = $$('.search_icon')[1]
+searchInput.onfocus = function() {
+    clearInput.classList.add('dsOpen')
+}
+clearInput.onclick = function() {
+    searchInput.value = ''
+    clearInput.classList.remove('dsOpen')
+}
 // HANDLE PLAYLIST-------------------------------------------------------------------------------------------------------------
 
 const artistAvatar = $('.artist_avatar')
 const artistName = $('.artist_name')
 const musicTitle = $('.music_title')
 
+const PLAYER = 'okenoc-Listen'
 const timeEndMusic = $('.timeend_music')
 const timeStartMusic = $('.timestart_music')
 
@@ -44,10 +56,23 @@ const backBtn = $('.backBtn')
 const randomBtn = $('.randomBtn')
 const repeatBtn = $('.repeatBtn')
 
+const volumeMute = $('.volumeMute')
+const volumeS = $('.volumeS')
+const volumeM = $('.volumeM')
+const volumeL = $('.volumeL')
+const rangeVolumeMusic = $('.volumeMusic')
+
+
 const playlistNext = $('.playlist_next-list')
 const app = {
     isRepeat: false,
     isRandom: false,
+    isPlay: false,
+    config: JSON.parse(localStorage.getItem(PLAYER))  || {},      
+    setConfig(key, value){
+        this.config[key] = value
+        localStorage.setItem(PLAYER, JSON.stringify(this.config))
+    },  
     currentIndex : 0,
     songs : [
         {
@@ -109,10 +134,11 @@ const app = {
         
     
     ],
+    
     render(){
-        const htmls = this.songs.map(song => {
+        const htmls = this.songs.map((song, index) => {
             return `
-            <li class="playlist_next-item row">
+            <li class="playlist_next-item row ${index === 0 ? 'songActive' : ''}" data-index="${index}">
                 <div class="playlist_next-description col l-6 m-6">
                     <div class="playlist_next-count">${song.count}</div>
                     <img class="playlist_next-img" src="${song.image}" alt="">
@@ -130,14 +156,9 @@ const app = {
             </li>`
         }).join('')
         playlistNext.innerHTML = htmls
-        const songActive = $$('.playlist_next-item')
-        songActive.forEach(function(songActive, index){
-            songActive.onclick = function() {
-                app.currentIndex = index  
-                app.loadCurrentSong() 
-                audio.play()            
-            }
-        })
+
+       
+        
         
     },
     defineProperties() {
@@ -182,19 +203,29 @@ const app = {
         // xử lý next song khi audio ended
         audio.onended = function () {
             if(!app.isRepeat){
-                app.nextSong()
+                nextBtn.click()
             }
             audio.play()
         }
         // xử lý khi click play
         playBtn.onclick = function() {
-            audio.play()            
+            app.isPlay = !app.isPlay
+            audio.play()          
         }
         // xử lý khi click pause
         pauseBtn.onclick = function() {
+            app.isPlay = !app.isPlay
             audio.pause()
         }
-
+        // Xử lý khi ấn space
+        document.onkeydown = function (e){
+            if(e.which === 32 && app.isPlay) {
+                pauseBtn.click()
+            }
+            // if(e.which === 32) {
+            //     playBtn.click()
+            // }
+        }
         // xử lý khi click next
         nextBtn.onclick = function() {
             if(app.isRandom){
@@ -219,30 +250,69 @@ const app = {
         randomBtn.onclick = function() {
             app.isRandom = !app.isRandom
             randomBtn.classList.toggle('active', app.isRandom)
-            // audio.play()
+            app.setConfig('isRandom', app.isRandom)
         }
         // Xử lý khi click repeat
         repeatBtn.onclick = function() {
             app.isRepeat = !app.isRepeat
             repeatBtn.classList.toggle('active', app.isRepeat)
+            app.setConfig('isRepeat', app.isRepeat)
+        }
+        // xử lý khi click vào chỗ bất kỳ trong playlistNext (dùng closest: tìm ra cha hoặc con)
+        playlistNext.onclick = function(e) 
+        {   const songNodeNoActive = e.target.closest('.playlist_next-item:not(.songActive)')
+
+            // Xử lý play khi ấn vào bài hát bất kỳ trong playlistNext (không tính song active)
+            if(songNodeNoActive) {
+                app.currentIndex = songNodeNoActive.dataset.index
+                app.loadCurrentSong()
+                audio.play()
+            }
+
+            const songNode = e.target.closest('.playlist_next-item')
+            $('.songActive').classList.remove('songActive')
+            songNode.classList.add('songActive')
         }
 
-        // xử lý khi click song active
-        // songActive.forEach(function(songActive){
-        //     songActive.onclick = function(e) {
-        //         e.target.classList.toggle('active')
-        //     }
-        // })
-       
         // khi time tiến độ bài hát thay đổi
         audio.ontimeupdate = function() {
-            rangeMusic.value = ((audio.currentTime / audio.duration) * 100)            
+            rangeMusic.value = ((audio.currentTime / audio.duration) * 100)  
+            app.setConfig('rangeMusic', rangeMusic.value)          
         }
-        
         // khi rangeMusic thay đổi 
         rangeMusic.onchange = function(e) {
             audio.currentTime = (e.target.value / 100) * audio.duration
+            app.setConfig('currentTime', audio.currentTime)
         }
+        
+        // xử lý khi kéo thanh âm lượng
+        audio.volume = 0
+        rangeVolumeMusic.onchange  = function() {
+            audio.volume = rangeVolumeMusic.value / 100 
+            app.setConfig('volumeMusic', audio.volume)
+            if(audio.volume == 0) {
+                $('.volumeActive').classList.remove('volumeActive')
+                volumeMute.classList.add('volumeActive')
+            }
+            if(audio.volume > 0.01 && audio.volume <= 0.35 ) {
+                $('.volumeActive').classList.remove('volumeActive')
+                volumeS.classList.add('volumeActive')
+            }
+            if(audio.volume > 0.35 && audio.volume <= 0.7 ) {
+                $('.volumeActive').classList.remove('volumeActive')
+                volumeM.classList.add('volumeActive')
+            }
+            if(audio.volume > 0.7 && audio.volume <= 1 ) {
+                $('.volumeActive').classList.remove('volumeActive')
+                volumeL.classList.add('volumeActive')
+            }
+        }
+        // xử lý khi âm lượng bài hát thay đổi
+        audio.onvolumechange = function() {
+            rangeVolumeMusic.value = audio.volume * 100
+            app.setConfig('rangeVolumeMusic', rangeVolumeMusic.value)
+        }
+        
     },
 
     loadCurrentSong(){
@@ -257,9 +327,20 @@ const app = {
         linkMusic.textContent = this.currentSong.name
         timeMusic.textContent = this.currentSong.time
         
-        audio.src = this.currentSong.path
-        
-        
+        audio.src = this.currentSong.path  
+    },
+    loadConfig(){
+        // Random / repeat
+        this.isRandom = this.config.isRandom
+        this.isRepeat = this.config.isRepeat
+        randomBtn.classList.toggle('active', app.isRandom)
+        repeatBtn.classList.toggle('active', app.isRepeat)
+        // Lưu tiến độ bài hát
+        rangeMusic.value = this.config.rangeMusic
+        audio.currentTime = this.config.currentTime
+        // Lưu âm lượng bài hát
+        audio.volume = this.config.volumeMusic
+        rangeVolumeMusic.value = this.config.rangeVolumeMusic
         
     },
     nextSong(){
@@ -297,6 +378,9 @@ const app = {
         
         //Tải thông tin bài hát đầu tiên vào UI khi chạy ứng dụng
         this.loadCurrentSong()
+
+        //Tải cấu hình từ config vào ứng dụng
+        this.loadConfig()
 
         // Render playlist
         this.render()
